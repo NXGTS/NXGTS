a552eb9f136757b57ec99d7c16fa7037d49e43b4
srini <shrinivas.springs@gmail.com>
2018-02-06 23:01:41 -0600

    This is the latest commit

--- MyNewProject-sairam/src/MyFirstClass.java
+++ MyNewProject-sairam/src/MyFirstClass.java
@@ -4,6 +4,8 @@ public class MyFirstClass {
 	public static void main(String[] args) {
     
 		System.out.println("Welcome to world of java");
+		// this is the edit after committing
+		//This is the latest code edit
 
 	}
 
